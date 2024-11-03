526cbead0482c1468603a3bd4f2cecb0f4f673ec
Ross Barnowski <rossbar@berkeley.edu>
2024-11-03 11:57:46 -0800

    Add download caching with rudimentary file registry.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
 """Utilities for model/data access."""
 
 _latest = "v0.1"
+_model_registry = {
+    "v0.1": "d98d7333dd00d37608180892a5e88d54"
+}
 
 
 def download_model(*, version=None):
@@ -20,4 +23,6 @@ def download_model(*, version=None):
     version = version if version is not None else _latest
     asset_key = f"models/deepcell-types_combined_ct_{version}.pt"
 
-    fetch_data(asset_key, cache_subdir="models")
+    fetch_data(
+        asset_key, cache_subdir="models", file_hash=_model_registry.get(version)
+    )
